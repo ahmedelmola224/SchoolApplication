65f7d3bf0fc63802a1efdb8628109f2d6e7edfe9
Ahmed Essam <72629680+ahmedelmola224@users.noreply.github.com>
2022-09-26 16:47:19 +0200

    Add files via upload

---
+++ School Application/OutLine/outline.h
@@ -0,0 +1,14 @@
+#ifndef OUTLINE_H_
+#define OUTLINE_H_
+#ifdef outlinefile
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "terminal.h"
+#endif
+void modeOutline1(int stdnumber);
+void modeOutline2();
+void modeOutline3();
+
+#endif
