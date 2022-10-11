b0427a0f31544fc15fcf4794ca681ad0e54d586a
Ahmed Essam <72629680+ahmedelmola224@users.noreply.github.com>
2022-10-11 22:30:23 +0200

    Update subject.h

--- School Application/Subject/subject.h
+++ School Application/Subject/subject.h
@@ -13,11 +13,11 @@ typedef struct subj_t
 	int degree[maxOfSubjects];/*array of student's degrees in each subject*/
 }subj_t;
 
-void initSubject(int n);/*is used to read from the database and intialize the array with it*/
-void terminateSubject(int n);/*is used to update  the database with the new values in the array*/
-void printSubject(const int id,int size);/*is used to take the id as the index of the array of degrees that needed to be printed */
-void scanSubject(const int id);/*is used to scan the degrees of subjects of new student*/
-void scanSubjecttForEdit(const int id);/*is used to to scan the modificated degrees student*/
-void swapSubjects(const int a ,const int b);/*is used to swap bewtwwn to elements of type subj_t*/
-void deleteSubject(const int index,const int size);/*is used to delete the degrees of a student*/
+void initSubject(int );/*is used to read from the database and intialize the array with it*/
+void terminateSubject(int );/*is used to update  the database with the new values in the array*/
+void printSubject(const int ,int );/*is used to take the id as the index of the array of degrees that needed to be printed */
+void scanSubject(const int );/*is used to scan the degrees of subjects of new student*/
+void scanSubjecttForEdit(const int );/*is used to to scan the modificated degrees student*/
+void swapSubjects(const int  ,const int );/*is used to swap bewtwwn to elements of type subj_t*/
+void deleteSubject(const int ,const int );/*is used to delete the degrees of a student*/
 #endif
