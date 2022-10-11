3f9970e40c416aaaf11722847cfaeb90b9da5451
Ahmed Essam <72629680+ahmedelmola224@users.noreply.github.com>
2022-10-11 22:29:30 +0200

    Update student.h

--- School Application/Student/student.h
+++ School Application/Student/student.h
@@ -12,33 +12,39 @@
 #include "subject.h"/*to see the subject layer */
 #endif
 
-#define maxOfStudent 100  /*seting the max number of students*/
-#define maxOfStdName 20  /*seting the max student name*/
-#define maxOfPhone 12   /*seting the max student phone*/
-#define maxClassNumber 4 /*seting the max student class*/
-#define checkIndex(index,size) (((index)<(size))&&((index)>=0)) /*macro to check the id*/
+#define NAMEMAX 20 /*seting the max student name*/
+#define PHONEMAX 12   /*seting the max student phone*/
+#define CLASSNAMEMAX 4 /*seting the max class name*/
+#define CHECKINDEX(index,size) (((index)<(size))&&((index)>=0)) /*macro to check the id*/
 
 extern int flag;/*flag to know which function is the calee*/
 
 typedef struct
 {
 	int id; 
-	char name[maxOfStdName];
+	char name[NAMEMAX];
 	unsigned char age;
 	char grade;
 	char gender;
-	char phone[maxOfPhone];
-	char class[maxClassNumber];
+	char phone[PHONEMAX];
+	char class[CLASSNAMEMAX];
 }stu_t;
 
-void initStudent(int n);/*is used to read the data from the database */
-void terminateStudent(int n);/*is used to write the data in the database */
-void printStudent(const int index,int size);/*is used to print student inforamtion*/
-int lookForStudent(const int size);/*is used to search for  student */
-void scanStudent(const int index);/*is used to scan new student*/
-void scanStudentForEdit(const int index);/*is used to scan the modifications*/
-int scanCheckID(const int size);/*is used to scan id and check it*/
-void callStudent(const int size);/*is used to call student*/
-void swapStudent(const int a,int b);/*is used to swap two student*/
-int deleteStudent(const int size);/*is used to delete student*/
+typedef enum 
+{
+	STUDENT_FOUNDED,
+	INVALID_ID,
+	INVALID_NAME,
+}student_Error_t;
+
+void initStudent(int );/*is used to read the data from the database */
+void terminateStudent(int );/*is used to write the data in the database */
+void printStudent(const int ,int );/*is used to print student inforamtion*/
+student_Error_t lookForStudent(const int ,int *);/*is used to search for  student */
+void scanStudent(const int );/*is used to scan new student*/
+void scanStudentForEdit(const int );/*is used to scan the modifications*/
+student_Error_t scanCheckID(const int ,int *);/*is used to scan id and check it*/
+void callStudent(const int );/*is used to call student*/
+void swapStudent(const int ,const int );/*is used to swap two student*/
+student_Error_t deleteStudent(const int );/*is used to delete student*/
 #endif
