01eb17398a2d07e3f2887d1e2f681f84d7da45ca
Ahmed Essam <72629680+ahmedelmola224@users.noreply.github.com>
2022-10-11 22:26:28 +0200

    Update school.h

--- School Application/School/school.h
+++ School Application/School/school.h
@@ -12,26 +12,34 @@
 #include "student.h"/*to see the student layer */
 #endif
 
-#define maxOfStudent 100 /*to set the max allowable number of the students in the school*/
+#define MAXSTUDENTS 100 /*to set the max allowable number of the students in the school*/
+#define NAMEMAX 20 /*to set the max allowable number of char in name*/
 
 /*a new type to define the school*/
 
 typedef struct school
 {
-	char name[20];/*the name of the school*/
+	char name[NAMEMAX];/*the name of the school*/
 	int stdnumber;/*current number of the students in the school*/
-	int std[maxOfStudent];/*array that have intgers that refers to IDS*/
+	int std[MAXSTUDENTS];/*array that have intgers that refers to IDS*/
 }school_t;
+
+typedef enum 
+{
+	APPROVED_OPERATION,
+	DECLINED_OPERATION
+	
+}school_Error_t;
 
 void initSchool();/*is used to read the data from the database */
 void terminateSchool();/*is used to write the data in the database */
 int getNumberOfStudent();/*is used to return the current number of the students */
 void printSchool();/*is used to print all the students in the school*/
-int printSpecificStudent();/*is used to print specifin student in the school*/
-int addStudentToSchool();/*is used to add new student to the school*/
-int checkStudentInSchool();/*is used to check if this student in the school*/
+school_Error_t printSpecificStudent();/*is used to print specifin student in the school*/
+school_Error_t addStudentToSchool();/*is used to add new student to the school*/
+school_Error_t checkStudentInSchool(int *);/*is used to check if this student in the school*/
 void editStudentInSchool();/*is used to edit student information*/
 void callStudentInSchool();/*is used to call student in the school*/
-int deleteStudentInSchool();/*is used to delete student from the school*/
+school_Error_t deleteStudentInSchool();/*is used to delete student from the school*/
 
 #endif
