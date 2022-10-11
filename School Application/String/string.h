8643c5728d552f5fb12701d520281573dfa440b1
Ahmed Essam <72629680+ahmedelmola224@users.noreply.github.com>
2022-10-11 22:27:23 +0200

    Update string.h

--- School Application/String/string.h
+++ School Application/String/string.h
@@ -1,8 +1,8 @@
 
 #ifndef STRING_H_
 #define STRING_H_
-void strScan(char *str,int size);
-int str_length(char *str);
-char ToLower(char a,char b);
-int str_compare(char *str1,char *str2);
+void strScan(char *,int );
+int str_length(char *);
+char ToLower(char ,char );
+int str_compare(char *,char *);
 #endif
