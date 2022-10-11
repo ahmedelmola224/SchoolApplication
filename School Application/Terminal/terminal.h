556c62ecef2fe9c288ff004dba227fa5b3551fb0
Ahmed Essam <72629680+ahmedelmola224@users.noreply.github.com>
2022-10-11 22:30:45 +0200

    Update terminal.h

--- School Application/Terminal/terminal.h
+++ School Application/Terminal/terminal.h
@@ -9,6 +9,6 @@
 
 #endif
 
-void delay(int milli_seconds);/*is used to stay in line for specific time*/
-void gotoxy(int x,int y);/*is used to move to specfic (x,y) coordinates on the screen*/
+void delay(int );/*is used to stay in line for specific time*/
+void gotoxy(int ,int );/*is used to move to specfic (x,y) coordinates on the screen*/
 #endif
